bb1e8e29800e1c3d768cd7fecbb9b6b82ba0aa78
Mateus Nogueira <mnogueira030@gmail.com>
2024-08-20 18:51:30 -0300

    Metodos
    criação de métodos básicos e acessando os mesmos de outra classe.

--- Primeiro Projeto/Assets/Scripts/PrimeiroScript.cs
+++ Primeiro Projeto/Assets/Scripts/PrimeiroScript.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class PrimeiroScript : MonoBehaviour
 {
+    public Car car;
 
     public float position;
 
     // Start is called before the first frame update
     void Start()
     {
-        position = PlayerPrefs.GetFloat("POSICAO", 1);
+        //position = PlayerPrefs.GetFloat("POSICAO", 1);
 
         //position = position + 1;
-        position++;
+        //position++;
 
-        PlayerPrefs.SetFloat("POSICAO", position);
+        //PlayerPrefs.SetFloat("POSICAO", position);
+
+        car.ChangeColor();
     }
 
     // Update is called once per frame
